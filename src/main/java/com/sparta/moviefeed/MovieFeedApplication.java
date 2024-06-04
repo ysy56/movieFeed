a1fdb023adb4be781d224b2559a395e0e044a2c3
Rohtable <rohtable@gmail.com>
2024-06-04 19:44:21 +0900

    init

---
+++ src/main/java/com/sparta/moviefeed/MovieFeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.moviefeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/moviefeed/MovieFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.moviefeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
