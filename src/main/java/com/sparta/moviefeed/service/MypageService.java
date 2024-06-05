d54d4568e5e8bfdc28418e7e39377d18ebfc8b83
moony <78634780+ysy56@users.noreply.github.com>
2024-06-05 15:47:49 +0900

    feat: 마이페이지 조회 기능 구현

---
+++ src/main/java/com/sparta/moviefeed/service/MypageService.java
@@ -0,0 +1,36 @@
+package com.sparta.moviefeed.service;
+
+import com.sparta.moviefeed.dto.responsedto.MypageResponseDto;
+import com.sparta.moviefeed.entity.User;
+import com.sparta.moviefeed.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MypageService {
+    private static final Logger log = LoggerFactory.getLogger(MypageService.class);
+    // security 사용X : 테스트를 위한 코드
+    private final Long userId = 1L;
+
+    private final UserRepository userRepository;
+
+    public MypageService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public MypageResponseDto getMypage() {
+//        // security 사용
+//        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        User user = userDetails.getUser();  // 여기서 user를 가져옵니다.
+
+        // security 사용X : 테스트를 위한 코드
+        User user = userRepository.findById(userId).orElseThrow(
+            () -> new IllegalArgumentException("해당 댓글은 존재하지 않습니다.")
+        );
+
+        return new MypageResponseDto(user);
+    }
+
+
+}
