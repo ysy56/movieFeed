458d22bcf7a63359e125bfb748025d0c900fbd0c
moony <78634780+ysy56@users.noreply.github.com>
2024-06-14 11:34:57 +0900

    test : Board 객체 생성 테스트 추가

--- src/main/java/com/sparta/moviefeed/dto/requestdto/BoardRequestDto.java
+++ src/main/java/com/sparta/moviefeed/dto/requestdto/BoardRequestDto.java
@@ -6,4 +6,9 @@ import lombok.Getter;
 public class BoardRequestDto {
     private String title;
     private String content;
+
+    public BoardRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
---
+++ src/test/java/com/sparta/moviefeed/entity/BoardTest.java
@@ -0,0 +1,38 @@
+package com.sparta.moviefeed.entity;
+
+import com.sparta.moviefeed.dto.requestdto.BoardRequestDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+    private BoardRequestDto boardRequestDto;
+    private Board board;
+
+    private String title;
+    private String content;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        title = "Test Title";
+        content = "Test Content";
+
+        boardRequestDto = new BoardRequestDto(
+                title,
+                content
+        );
+
+        board = new Board(boardRequestDto, user);
+    }
+
+    @Test
+    @DisplayName("객체 생성 확인")
+    void testUserCreation() {
+        assertEquals(title, board.getTitle());
+        assertEquals(content, board.getContent());
+        assertEquals(user, board.getUser());
+    }
+}
