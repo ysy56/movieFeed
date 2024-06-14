9f9cb384200907f9a01fffadd5fff868ce28945a
moony <78634780+ysy56@users.noreply.github.com>
2024-06-14 11:43:54 +0900

    test : Comment 객체 생성 테스트 추가

--- src/main/java/com/sparta/moviefeed/dto/requestdto/CommentRequestDto.java
+++ src/main/java/com/sparta/moviefeed/dto/requestdto/CommentRequestDto.java
@@ -1,8 +1,13 @@
 package com.sparta.moviefeed.dto.requestdto;
 
+import com.sparta.moviefeed.dto.responsedto.CommentResponseDto;
 import lombok.Getter;
 
 @Getter
 public class CommentRequestDto {
     private String content;
+
+    public CommentRequestDto(String content) {
+        this.content = content;
+    }
 }
---
+++ src/test/java/com/sparta/moviefeed/entity/CommentTest.java
@@ -0,0 +1,36 @@
+package com.sparta.moviefeed.entity;
+
+import com.sparta.moviefeed.dto.requestdto.CommentRequestDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentTest {
+    private CommentRequestDto commentRequestDto;
+    private Comment comment;
+
+    private String content;
+    private User user;
+    private Board board;
+
+    @BeforeEach
+    void setUp() {
+        content = "Test Content";
+
+        commentRequestDto = new CommentRequestDto(
+                content
+        );
+
+        comment = new Comment(commentRequestDto, board, user);
+    }
+
+    @Test
+    @DisplayName("객체 생성 확인")
+    void testUserCreation() {
+        assertEquals(content, comment.getContent());
+        assertEquals(board, comment.getBoard());
+        assertEquals(user, comment.getUser());
+    }
+}
