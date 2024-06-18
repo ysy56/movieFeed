6559e19997b47b2679185ec528f23aa18701f429
moony <78634780+ysy56@users.noreply.github.com>
2024-06-18 17:54:23 +0900

    test(/api/boards/{boardId}/comment) : 댓글 작성 요청 테스트 추가

--- src/main/java/com/sparta/moviefeed/dto/requestdto/CommentRequestDto.java
+++ src/main/java/com/sparta/moviefeed/dto/requestdto/CommentRequestDto.java
@@ -1,13 +1,13 @@
 package com.sparta.moviefeed.dto.requestdto;
 
-import com.sparta.moviefeed.dto.responsedto.CommentResponseDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class CommentRequestDto {
     private String content;
 
-    public CommentRequestDto(String content) {
+    public CommentRequestDto(@JsonProperty("content")  String content) {
         this.content = content;
     }
 }
