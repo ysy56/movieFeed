09ec2f0c79745429b8618d4df1f7dfcb9226dc5c
moony <78634780+ysy56@users.noreply.github.com>
2024-06-19 00:29:36 +0900

    fix : jsonProperty에 대한 400 error 해결

--- src/main/java/com/sparta/moviefeed/dto/requestdto/CommentRequestDto.java
+++ src/main/java/com/sparta/moviefeed/dto/requestdto/CommentRequestDto.java
@@ -1,13 +1,19 @@
 package com.sparta.moviefeed.dto.requestdto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sparta.moviefeed.dto.responsedto.CommentResponseDto;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class CommentRequestDto {
     private String content;
 
-    public CommentRequestDto(@JsonProperty("content")  String content) {
+    public CommentRequestDto() {
+    }
+
+    public CommentRequestDto(String content) {
         this.content = content;
     }
 }
